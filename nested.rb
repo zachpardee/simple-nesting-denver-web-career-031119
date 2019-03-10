a0dd93430cb40a654f2664aa15eed9dafd601ea4
awesome-edition-6075 <zachpardee@gmail.com>
2019-03-10 20:03:04 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -16,6 +16,7 @@ def hopper
         }
      }
 
+programmer_hash[:grace_hopper]
 
 end
 
@@ -37,6 +38,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +57,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     
+		 programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -66,6 +70,7 @@ def adding_matz
 
 	programmer_hash = 
  		{
+ 		  
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -79,7 +84,10 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+     
+		 programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby",
+		 :languages => ["LISP", "C"]}
+		 programmer_hash
     
 end
 
@@ -101,7 +109,9 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
+     		
+     programmer_hash[:alan_kay][:known_for] = "GUI"
+		 programmer_hash
      
 end
 
@@ -121,5 +131,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+		 programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+		 programmer_hash
 end
