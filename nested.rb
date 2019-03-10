0c889e4be78eb8bc45a3fee36ac2f1403020d7d2
awesome-edition-6075 <zachpardee@gmail.com>
2019-03-10 19:57:40 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -69,7 +69,7 @@ def adding_matz
  		   :yukihiro_matsumoto => {
         :known_for => "Ruby",
         :languages => ["LISP", "C"]
-}
+ 		   },
  		  
         :grace_hopper => {
           :known_for => "COBOL",
